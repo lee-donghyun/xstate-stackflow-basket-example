0466055736b23a3e25016612016cbf87b60369da
Donghyun Lee <leedonghyun022@gmail.com>
2024-08-16 13:58:14 +0900

    feat: 에셋 추가

--- src/activity/home.tsx
+++ src/activity/home.tsx
@@ -1,14 +1,32 @@
+import {
+  Bars2Icon,
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/24/outline";
 import { AppScreen } from "@stackflow/plugin-basic-ui";
 import { ActivityComponentType } from "@stackflow/react";
 
-import { Logo } from "../component/logo";
-
 export const Home: ActivityComponentType = () => {
   return (
-    <AppScreen appBar={{ renderLeft: Logo }}>
-      <div className="p-4">
-        <h1>StackFlow</h1>
-        <p>StackFlow is a simple and flexible navigation library for React.</p>
+    <AppScreen>
+      <div className="fixed inset-x-0 top-0 flex items-start gap-2 px-4">
+        <button className="mt-4">
+          <Bars2Icon className="size-6" />
+        </button>
+        <div>
+          <span className="mt-1 inline-block text-8xl font-medium leading-none tracking-tighter">
+            SxSS
+          </span>
+          <p className="ml-1 mt-1 text-sm font-light">Stackflow XState Shop</p>
+        </div>
+        <div className="mt-4 flex flex-1 justify-end gap-4">
+          <button>
+            <MagnifyingGlassIcon className="size-6" />
+          </button>
+          <button>
+            <ShoppingBagIcon className="size-6" />
+          </button>
+        </div>
       </div>
     </AppScreen>
   );
--- src/component/logo.tsx
+++
@@ -1,7 +0,0 @@
-export const Logo = () => {
-  return (
-    <span className="font-extrabold text-base inline-block px-2">
-      Stackflow XState Shop
-    </span>
-  );
-};
