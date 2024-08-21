9a954c15ef6116c16aef16934334f48650eac321
Donghyun Lee <leedonghyun022@gmail.com>
2024-08-21 09:15:32 +0900

    chore: tsc 개선

--- src/model/checkout.ts
+++ src/model/checkout.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { BasketItem } from "./basket-item";
 
-export const createCheckout = (items: BasketItem[]) =>
+export const createCheckout = (_items: BasketItem[]) =>
   new Promise<number>((resolve) => {
     setTimeout(() => {
       resolve(1);
